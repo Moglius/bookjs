9f8acba61c4b05056d10b93e4ff37e1eb7b9b3f6
Matias Moglia <matias.moglia@noname-it.com.ar>
2022-12-13 15:45:19 -0300

    working with dependency injection

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,12 +1,16 @@
 import { Book } from './../types/book';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, Output } from '@angular/core';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss']
 })
-export class BookComponent {
+export class BookComponent implements OnDestroy{
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   @Input() book: Book = {} as Book;
   @Output() bookEmitter = new EventEmitter<Book>();
